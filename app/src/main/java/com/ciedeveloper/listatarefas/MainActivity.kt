9aed9d6fa64ffd8003ae17145f539fc1da6746b0
Francisco S. Filho <franfellipe2@hotmail.com>
2024-09-07 09:41:53 -0300

    primerio comit

---
+++ app/src/main/java/com/ciedeveloper/listatarefas/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.ciedeveloper.listatarefas
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.SystemBarStyle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+       //enableEdgeToEdge(SystemBarStyle.auto(ContextCompat.getColor(this, R.color.primaryVariant), Color.TRANSPARENT))
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+}
